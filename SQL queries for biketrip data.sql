633208e89d84116d45443d3f9fc5f66a4129bb84
superramya <159821898+superramya@users.noreply.github.com>
2024-03-11 16:33:23 -0400

    SQL queries

---
+++ SQL queries for biketrip data.sql
@@ -0,0 +1,35 @@
+SELECT member_casual, day_of_week, COUNT(ride_id) as total_rides
+FROM biketrip_jandata
+GROUP BY member_casual, day_of_week;
+
+SELECT member_casual, COUNT(ride_id) as total_rides, day_of_week
+FROM biketrip_jandata
+WHERE day_of_week = 'Wednesday'
+GROUP BY member_casual, day_of_week;
+
+SELECT member_casual, day_of_week, 
+	COUNT(ride_id) OVER (PARTITION BY day_of_week) as total_rides
+FROM biketrip_jandata
+WHERE member_casual = 'member'
+
+SELECT member_casual, ride_len
+FROM biketrip_jandata
+WHERE member_casual = 'casual'
+
+SELECT member_casual,
+	CONVERT(VARCHAR(12), ride_len ,108) 'avg_len'
+FROM biketrip_jandata
+WHERE member_casual = 'casual'
+
+
+SELECT member_casual, day_of_week, COUNT(ride_id) as total_rides,
+CASE
+	WHEN day_of_week = 'Sunday' Then 'Funride'
+	WHEN day_of_week = 'Saturday' Then 'Lazyride'
+	ELSE 'Commute'
+END
+FROM biketrip_jandata
+GROUP BY day_of_week, member_casual
+ORDER BY total_rides
+
+
